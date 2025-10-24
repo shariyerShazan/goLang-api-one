726bef1d3a7daef4e0bed2933553f1f7075d6db8
shariyerShazan <shariyershazan1@gmail.com>
2025-10-25 01:47:29 +0600

    get api with params. and get url path

--- main.go
+++ main.go
@@ -9,9 +9,25 @@ import (
 func main() {
     app := fiber.New()
 
+	// server main path get
     app.Get("/", func (c *fiber.Ctx) error {
         return c.SendString("Hello, World!")
     })
+    
+	// params get
+    app.Get("/:userId?" , func (c *fiber.Ctx) error {
+          if c.Params("userId") != "" {
+            return c.SendString("User is is = " + c.Params("userId"))
+          } else {
+			return c.Status(fiber.StatusBadRequest).SendString("User ID is missing")
+		  }
+	  })     
 
+	//  api path get
+	app.Get("/api/*", func (c *fiber.Ctx) error {
+		return c.SendString("API path: " + c.Params("*"))
+	} )
+
+	
     log.Fatal(app.Listen(":3000"))
 }
