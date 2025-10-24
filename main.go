45a7e5238941691f9209112b0859662c5e3cb7cf
shariyerShazan <shariyershazan1@gmail.com>
2025-10-25 03:12:32 +0600

    version group in fiberGo

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "log"
+	// "errors"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
+	// "github.com/valyala/fasthttp/reuseport"
 )
 
 func main() {
@@ -13,9 +15,22 @@ func main() {
     app.Get("/", func (c *fiber.Ctx) error {
         return c.SendString("Hello, World!")
     })
+
+
+// version controll with api v1,v2 group
+	apiV1 := app.Group("/v1")
+	apiV1.Get("/" , func (c *fiber.Ctx) error {
+		return c.SendString("Version 1 is running")
+	})
+
+	apiv2 := app.Group("/v2")
+	apiv2.Get("/" , func (c *fiber.Ctx) error {
+		return c.SendString("Version 2 is runnign")
+	})
+
     
 	// params get
-    app.Get("/:userId?" , func (c *fiber.Ctx) error {
+    app.Get("user/:userId?" , func (c *fiber.Ctx) error {
           if c.Params("userId") != "" {
             return c.SendString("User is is = " + c.Params("userId"))
           } else {
@@ -30,8 +45,10 @@ func main() {
 
 
 	// statuc
-	app.Static("/" , "./public" ) // app.Static("api path" , "folder path" )
+	app.Static("/html" , "./public" ) // app.Static("api path" , "folder path" )
 	// and it's return the html file of that folder path
+
+
 
     log.Fatal(app.Listen(":3000"))
 	// app.Listen(":3000")
